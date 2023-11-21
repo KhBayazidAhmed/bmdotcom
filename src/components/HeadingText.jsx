1ec938b8fce709d472ad7679f7f2237087d997a5
kh bayazid ahmed <khandakerbayazidahmed@gmail.com>
2023-11-21 18:29:35 +0600

    added some component

---
+++ src/components/HeadingText.jsx
@@ -0,0 +1,23 @@
+function HeadingText() {
+  return (
+    <>
+      <div className=" text-center bg-gray-900 text-white  py-24 ">
+        <div className="flex flex-col text-center w-full mb-20"></div>
+        <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">
+          ROOF PARTY POLAROID
+        </h2>
+        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white  ">
+          Master Cleanse Reliac Heirloom
+        </h1>
+        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+          Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
+          gentrify, subway tile poke farm-to-table. Franzen you probably havent
+          heard of them man bun deep jianbing selfies heirloom prism food truck
+          ugh squid celiac humblebrag
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default HeadingText;
